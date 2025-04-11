a004c82111c0fd4afc67978e94814426aa7894f2
Genki Koyano <endrorukfine31@gmail.com>
2025-04-11 09:29:43 +0900

    🔥 不要なiconの削除

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,6 +3,7 @@ import { LocalCategoryData } from "../localData/LocalCategoryData";
 import Category from "./Category";
 
 const Categories = () => {
+  
   return (
     <div className="flex items-center p-8 flex-col">
       {LocalCategoryData.map((product, index) => (
